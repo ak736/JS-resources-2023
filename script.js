6aa4b2fcea9bbde809d845a77216f21d4efe431d
ak736 <as0700@srmist.edu.in>
2024-01-31 10:45:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -291,11 +291,11 @@ booksAfterUpdate
 
 */
 
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then((res) => res.json())
-//   .then((data) => console.log(data))
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then((res) => res.json())
+  .then((data) => console.log(data))
 
-// console.log('jonas')
+console.log('jonas')
 
 async function getTodos() {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
